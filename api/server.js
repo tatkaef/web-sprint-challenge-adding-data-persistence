23b0097d447d8a93efca32929e84b99649ff69f3
Tatiana Efimova <tatka_ef@mail.ru>
2020-12-11 10:55:47 -0800

    migration, seeds, knex data

--- api/server.js
+++ api/server.js
@@ -1,1 +1,19 @@
-// build your server here
+const express = require('express');
+const helmet = require('helmet');
+
+const db = require('./data/dbConfig.js');
+
+const server = express();
+
+const projectRouter = require('./project/project-router');
+const resourceRouter = require('./resource/resource-router')
+const taskRouter = require('./task/task-router')
+
+server.use(helmet());
+server.use(express.json());
+
+server.use('/api/projects', projectRouter);
+server.use('/api/resources/', resourceRouter);
+server.use('/api/projects', taskRouter)
+
+module.exports = server;
--- knexfile.js
+++ knexfile.js
@@ -1,8 +1,17 @@
 module.exports = {
   development: {
     // Complete.
+    client: 'sqlite3',
+      useNullAsDefault: true, // needed for sqlite
+      connection: {
+        filename: './data/projects.db3',
+        
+      },
     migrations: {
       directory: "./data/migrations",
+    },
+    seeds: {
+      directory: './data/seeds'
     },
   },
   testing: {
